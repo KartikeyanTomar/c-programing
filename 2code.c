d59cd3a3b18e69771166404062b4a5153c0cb7fb
Kartikeyan singh tomar <114302572+KartikeyanTomar@users.noreply.github.com>
2023-02-03 14:32:01 +0530

    Add files via upload

---
+++ 2code.c
@@ -0,0 +1,10 @@
+//wirte a c program to enter two numbers and print their sum
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("%d",c);
+    return 0;
+}
