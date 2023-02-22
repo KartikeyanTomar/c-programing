11403b1e55e89bbf3cfaf221c0ccb51e5401f312
Kartikeyan singh tomar <114302572+KartikeyanTomar@users.noreply.github.com>
2023-02-22 15:51:09 +0530

    Update code1.c

--- sheet's/sheet2/code1.c
+++ sheet's/sheet2/code1.c
@@ -1,16 +1,30 @@
-// write a c program to find maximum between two numbers using conditional operator
-#include <stdio.h>
+// write a c program to find maximum between three numbers using conditional operator.
+# include <stdio.h>
+# include <math.h>
 int main()
 {
-    int a, b;
-    scanf("%d%d", &a, &b);
-    if (a<b)
-    {
-        printf("maximum number is %d",b);
-    }
-    else
-    {
-        printf("maximum number is %d",a);
-    }
+    int a,b,c;
+    scanf("%d%d%d", &a,&b,&c);
+    a>b && a>c ? printf("Maximum is %d",a): b>c ? printf("Maximum is %d",b) : printf("Maximum is %d",c);
     return 0;
 }
+
+/*
+By if else
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if (a>b && a>c)
+    {
+        printf("max no. is %d",a);
+    }
+    else if (b>a && b>c)
+    {
+        printf("max no. is %d",b);
+    }
+    else if (c>a && c>b)
+    {
+        printf("max no. is %d",c);
+    }
+    return 0 ;
+}*/
