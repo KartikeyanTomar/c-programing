85c51d2a3a652d127c2a401fc2826f10caa6da3d
Kartikeyan singh tomar <114302572+KartikeyanTomar@users.noreply.github.com>
2023-02-22 16:03:56 +0530

    Update code5.c

--- sheet's/sheet2/code5.c
+++ sheet's/sheet2/code5.c
@@ -2,6 +2,19 @@
 # include <stdio.h>
 int main()
 {
+    char a,b;
+    scanf("%c", &a);
+    b=("%d",a);
+    (b>=65 && b<=90) || (b>=97 && b<=122) ? printf("It is an alphabet") : printf("It is not an alphabet");
+    return 0;
+}
+
+
+
+
+/*{
+    by if else
+    
     int a,b;
     scanf("%c",&a);
     b=("%d",a);
@@ -15,3 +28,4 @@ int main()
     }
     return 0;
 }
+*/
