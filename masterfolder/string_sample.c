a4a0c97b16d55223956ab8c9d70c2490d1366ebf
Kartikeyan singh tomar <114302572+KartikeyanTomar@users.noreply.github.com>
2023-05-22 00:07:28 +0530

    Add files via upload

---
+++ masterfolder/string_sample.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+void main()
+{   
+        char arr[1000]={'M', 'A', 'A', 'M', '\0'};
+        char name[100]="Amir";
+
+        
+        printf("%s", arr);
+
+        
+}
---
+++ masterfolder/test_file.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+// void main()
+// {
+//     int a=2,p;
+//     p= 1==2!=3;
+//     printf("value of p %d\n",p);
+//     printf("Value of a %d", a);
+// }
+
+
+
+int main() 
+{
+   
+    // printf("%c", c);
+    int a;
+    for(a=0;;)
+    printf("hello");
+
+}
